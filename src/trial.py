1b2b800ada5061251b29a2c953b87afae920bdb3
Masiha Tasneem <masihatasneem99@gmail.com>
2026-05-31 16:03:05 +0530

    Removed trial.py

--- src/trial.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data/Resume/Resume.csv")
-print(df.shape)          # should be ~(2484, 4)
-print(df.columns.tolist())  # should be ['ID', 'Resume_str', 'Resume_html', 'Category']
-print(df["Category"].unique())  # should show clean labels like 'Data Science', 'HR', etc.
